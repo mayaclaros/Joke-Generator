ec9783ea7d8c50ab55374a3d4651dcb146c2864e
mayaclaros <123900092+mayaclaros@users.noreply.github.com>
2024-08-25 16:46:59 -0400

    update index.js
    added axio and function

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 function showAnswer(response) {
-  console.log(response);
+  alert(response.data.answer);
 }
-
-let apiKey = "24b9b3ae0f6ea728ao45f6et261b0962";
-let context = "be kind and funny";
-let prompt = "Tell a funny joke";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=Q${apiKey}`;
-
-axios.get(apiUrl).then(showAnswer);
 
 function handleClick() {
-  alert("hooray");
+  axios.get(apiUrl).then(showAnswer);
 }
+let apiKey = "24b9b3ae0f6ea728ao45f6et261b0962";
+let prompt =
+  "Please tell me a new funny joke every time a response is requested";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&key=${apiKey}`;
 
 let buttonElement = document.querySelector("#special-button");
 buttonElement.addEventListener("click", handleClick);
